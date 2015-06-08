490ed06964615cecf9f960293cf69b61352450d9
幻梦飞羽 <Lds18267831681>
2015-06-08 09:40:35 +0800

    15-6-8

--- RentalWebSocket/Command/ManuallyArm.cs
+++ RentalWebSocket/Command/ManuallyArm.cs
@@ -1,4 +1,5 @@
-﻿using SuperWebSocket.SubProtocol;
+﻿using RentalUtity;
+using SuperWebSocket.SubProtocol;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +21,24 @@ namespace RentalWebSocket.Command
         {
             try
             {
+                OperateModel operate = new OperateModel();
+                operate.commandID = 0x0205;
+                operate.deviceId = Convert.ToUInt32(commandList.StationNo);
+                List<byte> byteList = new List<byte>();
+                string a = commandList.HostID;
 
+                byteList.AddRange(Encoding.GetEncoding("gbk").GetBytes(a));
+                byteList.Add(Convert.ToByte(commandList.type));
+                operate.Data = byteList.ToArray();
+                operate.sessionId = session.SessionID;
+                operate.Sn = Convert.ToUInt16(commandList.Key);
+                operate.comid = Name;
+
+                RentalServer.operateList.Enqueue(operate);
             }
             catch (Exception ex)
             {
-
+                Log.Error(ex.ToString());
             }
         }
     }
